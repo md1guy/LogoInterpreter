1eec09cc313a95c7658880311ab9139bbb63ee16
Metyslav <volchkov.oleh@gmail.com>
2018-11-10 18:32:21 +0200

    code formatting(newlines and several whitespaces to single whitespace)

--- parser.js
+++ parser.js
@@ -1,4 +1,20 @@
+let Command = require("./command.js")
+
 class Parser {
 
-    
+    parse(code) {
+        if (typeof(code) !== "string") {
+            // wrong parameter type exception
+        }
+
+        let newlineRegex = /\r?\n|\r/g;
+        let severalWhitespacesRegex = /\s{2,}/g
+
+        code = code.replace(newlineRegex, ' ');
+        code = code.replace(severalWhitespacesRegex, ' ');
+
+        return code;
+    }
 }
+
+module.exports = Parser;
