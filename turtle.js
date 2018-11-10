ed90245da17bd821a419319f2a6d4b3e9caaa4dd
Metyslav <volchkov.oleh@gmail.com>
2018-11-10 18:11:24 +0200

    Added 'penDown' property to Turtle class

--- turtle.js
+++ turtle.js
@@ -1,7 +1,8 @@
 class Turtle {
-    constructor(x, y, dir) {
+    constructor(x = 0, y = 0, dir = 0) {
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.penDown = true;
     }
 }
