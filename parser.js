fd23be9fbf76fdde535731fb83495ef3e197c190
Metyslav <volchkov.oleh@gmail.com>
2018-11-10 19:00:16 +0200

    Implemented code parsing(base commands with or without args)

--- parser.js
+++ parser.js
@@ -3,14 +3,38 @@ let Command = require("./command.js")
 class Parser {
 
     static parse(code) {
+
         if (typeof(code) !== "string") {
-            // wrong parameter type exception
+            // TODO: wrong parameter type exception
         }
         
-        return "asfe";
+        code = Parser.format(code);
+
+        let tokens = code.split(' ');
+
+        let index = 0;
+
+        let commands = [];
+
+        let digitRegex = /\d+/;
+
+        while(index < tokens.length) {
+
+            let name = tokens[index];
+            let args = [];
+
+            while (digitRegex.test(tokens[++index])) {
+                args.push(tokens[index]);
+            }
+
+            commands.push(new Command(name, args));
+        }
+
+        return commands;
     }
 
     static format(text) {
+
         let newlineRegex = /\r?\n|\r/g;
         let severalWhitespacesRegex = /\s{2,}/g
 
