078f26191fc8e5c717023e517e7f2a1f99088b95
Metyslav <volchkov.oleh@gmail.com>
2018-11-15 21:33:09 +0200

    changed app name and title on window

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 800,
+    title: "Logo Interpreter",
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('public/index.html')
